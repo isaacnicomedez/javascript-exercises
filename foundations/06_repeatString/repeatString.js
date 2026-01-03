68504e1a26cd83141879bc23f387c6517c75bb1a
Isaac <isaacnicomedez1@gmail.com>
2026-01-03 06:06:50 -0800

    Solve repeat string exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, repeat) {
+    let newString = '';
 
+    if (repeat < 0) return 'ERROR';
+
+    for (let i = 0; i < repeat; i++) {
+        newString += word;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line

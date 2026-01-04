d3df00fe8505c1fac58af02a8a66b69dd4dba5c3
Isaac <isaacnicomedez1@gmail.com>
2026-01-03 21:19:23 -0800

    Sum all integers with param arrangement validation

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !(Number.isInteger(start) && Number.isInteger(end))) return `ERROR`;
 
+    let highest = start < end ? end : start;
+    let lowest = start < end ? start : end;
+
+    let sum = 0;
+    for (let i=lowest; i<=highest; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

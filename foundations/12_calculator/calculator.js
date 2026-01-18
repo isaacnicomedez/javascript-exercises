a59e8fbae2bf24016dbd4685b55430e51fb1a812
Isaac <isaacnicomedez1@gmail.com>
2026-01-18 02:41:54 -0800

    Add subtract function

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -2,16 +2,16 @@ const add = function(num1, num2) {
 	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, num) => accumulator + num, 0);
 };
 
 const multiply = function() {
-
+  return arr.reduce((accumulator, num) => accumulator * num, 1);
 };
 
 const power = function() {

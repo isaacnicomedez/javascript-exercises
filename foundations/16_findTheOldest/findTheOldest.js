72e3e51ce604c0b1564a5035f1aa8017130e04e7
Isaac <isaacnicomedez1@gmail.com>
2026-01-18 06:00:18 -0800

    Finish object oldest value getter

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+function getDeath(person) {
+    return death = !person.yearOfDeath ? (new Date()).getFullYear() : person.yearOfDeath;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+        const current = getDeath(person) - person.yearOfBirth;
+
+        return current > getDeath(oldest) - oldest.yearOfBirth ? person : oldest; 
+    }, arr[0]);
 };
 
 // Do not edit below this line

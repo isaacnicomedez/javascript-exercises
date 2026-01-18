e10fbb2d35d9b067496efa1f7d6b1fc99690b8b6
Isaac <isaacnicomedez1@gmail.com>
2026-01-18 03:55:15 -0800

    Add palindromes function checker

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const newStr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => alphanumerical.includes(char))
+        .join("");
+
+    const reversedStr = newStr
+        .split('')
+        .reverse()
+        .join('');
+
+    return newStr === reversedStr;
 };
 
 // Do not edit below this line

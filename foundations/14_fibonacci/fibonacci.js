0342af1e17b0f93c838ccf1025cd3ad6078657e2
Isaac <isaacnicomedez1@gmail.com>
2026-01-18 04:39:47 -0800

    Add working fibonacci positive integers

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if (n === 0) return 0;
+    if (n < 0) return "OOPS";
 
+    const arr = [1, 1]
+    for (let i = 1; i < n-1; i++) {
+        arr.push(arr[i] + arr[i-1]);
+    }
+
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
